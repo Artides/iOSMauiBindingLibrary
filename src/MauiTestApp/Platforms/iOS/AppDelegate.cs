d5e3d66aad43d11ef5ece9fdece705091e726cbe
Cimmino, Matteo <matteo-giusepe.cimmino@lutech.it>
2024-05-09 10:22:33 +0200

    Added projects skeleton.

---
+++ src/MauiTestApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using CocoaHTTPServerBinding;
+
+namespace MauiTestApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        public AppDelegate()
+        {
+                
+        }
+    }
+}
